3a42a62dd096ed3201fe88f9d0ca399a123ba6ad
ToTu-Dev <75479355+ToTu-Dev@users.noreply.github.com>
2021-07-03 21:03:06 +0530

    Final Commit 🚀

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view />
+
+  <todolist />
 </template>
 
 <style lang="scss"></style>
+
+<script>
+import todolist from "./components/todolist.vue";
+export default {
+  components: { todolist },
+};
+</script>
